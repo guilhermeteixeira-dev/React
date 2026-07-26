91f1de3aa6f1869d8169e18b06e09219c48387fd
guilhermeteixeira-dev <gt13082000@outlook.com>
2026-07-26 17:49:35 -0300

    componentes css

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
+  const nome = "Maria";
 
   return (
     <div className="App">
-      <SayMyName nome="Guilherme" />
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
       <Pessoa 
         nome="Guilherme" 
         idade="26" 
         profissao="Programador" 
-        foto="" 
+        foto="https://images5.alphacoders.com/132/thumb-1920-1323847.jpg"
       />
     </div>
   )
--- meu-projeto/src/components/Pessoa.js
+++ meu-projeto/src/components/Pessoa.js
@@ -1,10 +1,11 @@
+import styles from './Frase.module.css';
 function Pessoa({nome, idade, profissao, foto}) {
     return (
         <div>
-            <img src={foto} alt={nome} />
+            <img src={foto} width="300px" alt={nome} />
             <h2>Nome: {nome}</h2>
-            <h3>Idade: {idade}</h3>
-            <h3>Profissão: {profissao}</h3>   
+            <p>Idade: {idade}</p>
+            <p>Profissão: {profissao}</p>
         </div>
     )
 }
